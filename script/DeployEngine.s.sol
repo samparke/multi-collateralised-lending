1719ec325b8b3d8c7a05e080daf228e57be4f74d
Samuel Parke <samueljparke@gmail.com>
2025-07-31 13:41:28 +0100

    deploy engine script

---
+++ script/DeployEngine.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+import {Coin} from "../src/Coin.sol";
+import {Engine} from "../src/Engine.sol";
+
+contract DeployEngine is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    /**
+     *
+     * @return returns the deployed engine. we pass in the token and pricefeed addresses from our config (which will be determined by the chainId),
+     * as well as the coin we deployed
+     * @return returns the coin, which is be used in our engine contract to mint, burn etc
+     * @return returns the config for testing
+     */
+    function run() external returns (Engine, Coin, HelperConfig) {
+        HelperConfig config = new HelperConfig();
+        (address weth, address wbtc, address wethUsdPriceFeed, address wbtcUsdPriceFeed, uint256 deployerKey) =
+            config.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
+        Coin coin = new Coin();
+        Engine engine = new Engine(priceFeedAddresses, tokenAddresses, coin);
+        coin.transferOwnership(address(engine));
+        vm.stopBroadcast();
+        return (engine, coin, config);
+    }
+}
